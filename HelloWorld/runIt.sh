29bf45dfbdc685781fa9024d553d06d8cbe105be
Brandon Krett <brandon.krett@accenture.com>
2018-09-12 17:27:35 -0500

    add file

---
+++ HelloWorld/runIt.sh
@@ -0,0 +1,2 @@
+appcenter apps set-current brandon-krett-7j1f/HelloWorld --token 7966e518a0b77b32597e9408c987b1e20a7187a1
+appcenter build queue -b master --token 7966e518a0b77b32597e9408c987b1e20a7187a1
